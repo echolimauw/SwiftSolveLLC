e61176fb074a08ff66a855ae9624acc2f454b9f4
echolimauw <49353272+echolimauw@users.noreply.github.com>
2019-11-28 15:25:56 -0800

    Add files via upload
    Added JavaScript to try to get submenus to drop down on hover. Doesn't work, but work in progress.

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function closeNav() {
     document.getElementById("mysidepanel").style.width = "0";
     // document.getElementById("main").style.marginLeft = "0";
     }
+
+function openMenu() {
+    var dropdown = document.getElementsByClassName("dropdown-links");
+    var i;
+
+    console.log("openmenu function reached")
+
+    for (i = 0; i < dropdown.length; i++) {
+        console.log("for loop reached")
+        dropdown[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } 
+            else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+}
